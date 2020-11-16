b4cda7f3b62cd9aac91b859974314d85e04ac96e
younganJung1115 <youngan.jung@gmail.com>
2020-11-16 06:25:59 +0000

    use tesseract js library

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 const { createWorker } = require('tesseract.js');
 
-const worker = createWorker({
-    logger: m => console.log(m)
-});
+const worker = createWorker();
 
 (async () => {
     await worker.load();
-    await worker.loadLanguage('eng+kor');
-    await worker.initialize('eng+kor');
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
     const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
     console.log(text);
     await worker.terminate();
