85da7155c6e9060f0973a0a6f96caeffb821640d
younganJung1115 <youngan.jung@gmail.com>
2020-11-16 06:22:30 +0000

    use tesseract

--- test.js
+++ test.js
@@ -1,51 +1,14 @@
-// Imports the Google Cloud client libraries
-const vision = require('@google-cloud/vision').v1;
+const { createWorker } = require('tesseract.js');
 
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
+const worker = createWorker({
+    logger: m => console.log(m)
+});
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// Bucket where the file resides
-const bucketName = 'yeah';
-// Path to PDF file within bucket
-// const fileName = 'path/to/document.pdf';
-// The folder to store the results
-// const outputPrefix = 'results'
-
-// const gcsSourceUri = `gs://${bucketName}/${fileName}`;
-const gcsSourceUri = `gs://cloud-samples-data/vision/pdf_tiff/census2010.pdf`;
-// const gcsDestinationUri = `gs://${bucketName}/${outputPrefix}/`;
-const gcsDestinationUri = `gs://ocr_read_text_from_pdf/`;
-
-const inputConfig = {
-    // Supported mime_types are: 'application/pdf' and 'image/tiff'
-    mimeType: 'application/pdf',
-    gcsSource: {
-        uri: gcsSourceUri,
-    },
-};
-const outputConfig = {
-    gcsDestination: {
-        uri: gcsDestinationUri,
-    },
-};
-const features = [{ type: 'DOCUMENT_TEXT_DETECTION' }];
-const request = {
-    requests: [
-        {
-            inputConfig: inputConfig,
-            features: features,
-            outputConfig: outputConfig,
-        },
-    ],
-};
-
-const kimchi = async () => {
-    const [operation] = await client.asyncBatchAnnotateFiles(request);
-    const [filesResponse] = await operation.promise();
-    const destinationUri = filesResponse.responses[0].outputConfig.gcsDestination.uri;
-    console.log('Json saved to: ' + destinationUri);
-}
-kimchi()
+(async () => {
+    await worker.load();
+    await worker.loadLanguage('eng+kor');
+    await worker.initialize('eng+kor');
+    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    console.log(text);
+    await worker.terminate();
+})();
