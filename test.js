f6b691000c83619b8c0cec536234ace0c302fc8d
younganJung1115 <youngan.jung@gmail.com>
2021-12-21 12:29:47 +0900

    test

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
-const { createWorker } = require('tesseract.js');
+const { createWorker } = require("tesseract.js");
 
 const worker = createWorker();
 
 (async () => {
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    console.log(text);
-    await worker.terminate();
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  const {
+    data: { text },
+  } = await worker.recognize(
+    "https://tesseract.projectnaptha.com/img/eng_bw.png"
+  );
+  console.log(text);
+  await worker.terminate();
 })();
+
+//test
